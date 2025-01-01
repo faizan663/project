76f93d92ea8318c39f12cf88997c1aa7af4e94c8
faizan663 <rajafaizan9979@gmail.com>
2025-01-01 23:49:20 +0500

    project

---
+++ assets/js/collection.js
@@ -0,0 +1,14 @@
+// document.querySelectorAll('.collection-btn .btn').forEach(button => {
+//     button.addEventListener('click', () => {
+//         const category = button.getAttribute('data-category');
+//         const cards = document.querySelectorAll('.collection-cards');
+
+//         cards.forEach(card => {
+//             if (category === 'all' || card.getAttribute('data-category') === category) {
+//                 card.style.display = 'block';
+//             } else {
+//                 card.style.display = 'none';
+//             }
+//         });
+//     });
+// });
